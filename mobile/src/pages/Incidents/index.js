fcc7af13f3a708838d427e6ded83e7471ff5f639
kaiqnes <kaiqnes@gmail.com>
2020-03-28 00:26:52 -0300

    Criação do App Mobile - To Be Hero!

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react'
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { Feather } from '@expo/vector-icons'
+
+import api from '../../services/api'
+
+import logoImg from '../../assets/logo.png'
+
+import styles from './styles'
+
+export default function Incidents() {
+  const [incidents, setIncidents] = useState([])
+  const [total, setTotal] = useState(0)
+  const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
+
+  const navigation = useNavigation()
+
+  function navigateToDetail(incident) {
+    navigation.navigate('Details', { incident })
+  }
+
+  async function loadList() {
+    if (loading) {
+      return
+    }
+
+    if (total > 0 && incidents.length === total) {
+      return
+    }
+
+    setLoading(true)
+
+    const resp = await api.get('incidents', {
+      params: { page }
+    })
+
+    setTotal(resp.headers['x-total-count'])
+    setIncidents([...incidents, ...resp.data])
+    setPage(page + 1)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    loadList()
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.HeaderTextBold}>{total} casos</Text>.
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>Escolha um dos casos abaixo e salve o mundo.</Text>
+
+      <FlatList
+        data={incidents}
+        style={styles.incidentList}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={incident => String(incident.id)}
+        onEndReached={loadList}
+        onEndReachedThreshold={0.2}
+        renderItem={({ item }) => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>{item.name}</Text>
+
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentValue}>{item.title}</Text>
+
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>{
+              Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
+                .format(item.value)
+            }</Text>
+
+            <TouchableOpacity style={styles.detailsButton} onPress={() => navigateToDetail(item)}>
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Feather name="arrow-right" size={16} color="#E02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  )
+}
+/*
+
+*/
